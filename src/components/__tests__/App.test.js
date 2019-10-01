788e5f814cd176319b7fa838a6418620eb30f4de
Nico <nico@skubos.com>
2019-10-01 16:59:46 +0700

    adds absolute paths to jsconfig

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import App from '../App';
-import CommentBox from '../CommentBox';
+import App from 'components/App';
+import CommentBox from 'components/CommentBox';
+import CommentList from 'components/CommentList';
 
 it('shows a comment box', () => {
   const wrapped = shallow(<App />);
-
   expect(wrapped.find(CommentBox).length).toEqual(1);
 });
+
+it('shows a comment list', () => {
+  const wrapper = shallow(<App />);
+  expect(wrapper.find(CommentList).length).toEqual(1);
+})
